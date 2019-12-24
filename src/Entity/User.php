2778e92213ba7df6a023d8665c67d9860277dfbe
vetal33 <v.bitko@gmail.com>
2019-12-24 11:19:58 +0200

    add RegistrationForm

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,11 +3,13 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Table(name="users")
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
 {
