6744aa3f0fefcd8c9abe43eaaae6aac7538f50c7
vetal33 <v.bitko@gmail.com>
2020-09-10 18:05:47 +0300

    add modify local factor

---
+++ tests/Service/ParcelJsonParserTest.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Tests\Service;
+
+
+use App\Service\ParcelHandler;
+use PHPUnit\Framework\TestCase;
+
+class ParcelHandlerTest extends TestCase
+{
+    public function testCalculateLocalsValue()
+    {
+        $parcelHandler = new ParcelHandler();
+        $parcelHandler->
+
+    }
+
+}
